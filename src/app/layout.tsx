f16b4c6915af2d5a5cb7be9f221e849d28931e9d
Shreyash Raj <shreyashr267@gmail.com>
2024-03-14 21:37:52 +0530

    updated css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + " container"}>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
